ac276524b2ee5ec86fec94503190ff3cfd288598
felipesantinho <felipeaugustosantinho@gmail.com>
2022-09-30 15:47:03 -0300

    exercise 014

---
+++ discover/ex014/process.js
@@ -0,0 +1,7 @@
+//lista de argumentos
+
+//console.log(process.argv)
+
+//console.log("meu nome é " + process.argv[2] + ' ' + process.argv[3] + ' ' + process.argv[4])
+
+console.log(process.argv)
---
+++ discover/ex014/require.js
@@ -0,0 +1,15 @@
+/*
+
+Modulos Nativos do Node JS
+
+const path = require('path')
+
+console.log(path.basename(__filename))
+
+*/
+
+//Meus Módulos
+
+const myModule = require('./exports')
+
+console.log(myModule)
